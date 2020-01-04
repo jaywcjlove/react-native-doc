5b214727b0a36e8d130cb4e32a1b522969316c63
jaywcjlove <398188662@qq.com>
2020-01-04 13:40:15 +0800

    Modify replace script.

--- script/replace.js
+++ script/replace.js
@@ -35,7 +35,7 @@ async function format(dir) {
     await fs.ensureDir(webPath);
     await fs.emptyDir(webPath);
     await fs.copy(rnPath, webPath, {
-      filter: (src) => !newFunction().test(src)
+      filter: (src) => !regDocPath().test(src) && !regGitPath().test(src) && /(\.circleci|)$/g.test(src)
     });
     await fs.copy(cdnPath, webCdnPath);
     await format(webPath);
@@ -44,6 +44,10 @@ async function format(dir) {
   }
 })();
 
-function newFunction() {
+function regGitPath() {
+  return /\/react\-native\/(\.git)(?=(\/|$))/;
+}
+
+function regDocPath() {
   return /\/react\-native\/docs\/(0.5|0.6|0.7|0.8|0.9|0.10|0.11|0.12|0.13|0.14|0.15|0.16|0.17|0.18|0.19|0.20|0.21|0.22|0.23|0.24|0.25|0.26|0.27|0.28|0.29|0.30|0.31|0.32|0.33|0.34|0.35|0.36|0.37|0.38|0.39|0.40|0.41|0.42|0.43|0.44|0.45|0.46|0.47|0.48|0.49|0.50|0.51|0.52|0.53|0.54|0.55|0.56|0.57|0.58|0.59)(?=(\/|$))/;
 }
